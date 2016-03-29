2924b2f95da8a6eee0d63aaae27d39fd64eda0c4
Topher <tophermade@gmail.com>
2016-03-29 17:53:04 -0600

    starting dev

---
+++ Assets/Scripts/Player.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var body 				: Rigidbody2D;
+
+var playing 			: boolean 		= true;
+
+var jumpPower 			: float 		= 7;
+
+
+function Jump(){
+	body.velocity.y = jumpPower;
+}
+
+
+function Start () {
+	body = GetComponent(Rigidbody2D);
+
+}
+
+function Update () {
+	if(playing){
+		if(Input.GetMouseButtonDown(0)){
+			Jump();
+		}
+	}
+}
