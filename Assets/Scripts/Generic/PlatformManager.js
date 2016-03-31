ca7d0ab2eb06f5720ef0de4ac05d987120ea16a7
Topher <tophermade@gmail.com>
2016-03-31 05:47:42 -0600

    ready for iap

---
+++ Assets/Scripts/Generic/PlatformManager.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+
+var hideOnAndroid 		: GameObject[];
+var hideOnIOS 			: GameObject[];
+var hideOnWeb 			: GameObject[];
+var hideOnDesktop 		: GameObject[];
+
+
+function Awake(){
+	var hideThese : GameObject[];
+	
+	#if UNITY_ANDROID
+		hideThese = hideOnAndroid;
+	#endif
+	
+	#if UNITY_IPHONE
+		hideThese = hideOnIOS;
+	#endif
+	
+	#if UNITY_WEBGL
+		hideThese = hideOnWeb;
+	#endif
+	
+	#if UNITY_STANDALONE_WIN
+		hideThese = hideOnDesktop;
+	#endif
+
+	for (var i = 0; i < hideThese.length; i++) {
+		hideThese[i].SetActive(false);
+	}
+}
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
