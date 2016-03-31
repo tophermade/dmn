1525f2274541e75181f464da6c978ed147378a31
Topher <tophermade@gmail.com>
2016-03-31 06:02:07 -0600

    added iap

---
+++ Assets/Scripts/Generic/SimpleShare.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var url : String 			= "http://dontmakenuggets.tk";
+var text : String 			= "I'm rocking it in Don't Make Nuggets for iOS and Android. You should too. ";
+
+function ShareOnFacebook(){
+	Application.OpenURL("http://www.facebook.com/sharer/sharer.php?u=" + url);
+}
+
+function ShareOnTwitter(){
+	Application.OpenURL("https://twitter.com/intent/tweet?text=" + text + url);
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
